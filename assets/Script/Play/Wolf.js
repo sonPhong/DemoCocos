c2ce0189ad7a9eb9b66497789ed023753440c7f6
sonPhong <125986340+sonPhong@users.noreply.github.com>
2025-05-29 18:06:35 +0700

    update

---
+++ assets/Script/Play/Wolf.js
@@ -0,0 +1,27 @@
+const EnemyBase = require("MonterBase");
+
+cc.Class({
+    extends: EnemyBase,
+
+    init(level = 1) {
+        this._super(level);
+        this.moveDuration = 4 + Math.random();
+    },
+
+    die() {
+        this._super();
+
+        // cc.tween(this.node)
+        //     .to(0.3, { angle: 90 })
+        //     .to(0.3, { opacity: 0, y: this.node.y - 30 })
+        //     .start();
+    },
+
+    onCollisionEnter(other,self){
+        if(other.node.group == 'EndScene'){
+            this.die();
+        }if(other.node.group == 'Sword'){
+            this.takeDamage(500);
+        }
+    },
+});
